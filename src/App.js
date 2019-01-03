f17136267dbcc1717173a28cb1e6e3501a579e5d
Tori Machen <tmachen@pillartechnology.com>
2019-01-03 16:37:33 -0600

    WIP: added the Provider and store, need to add reducer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './App.css';
 import Counter from './Counter';
+
+const store = createStore();
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Provider store={store}>
         <Counter />
-      </div>
+      </Provider>
     );
   }
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Counter extends Component {
     increment = () => {
@@ -21,4 +22,10 @@ class Counter extends Component {
     }
 }
 
-export default Counter;
+function mapStateToProps(state) {
+    return {
+        count: state.count
+    };
+}
+
+export default connect(mapStateToProps)(Counter);
